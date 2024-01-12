61ab4fe5156dd8089f71894a9c82741e00554c76
David <david281100@hotmail.com>
2024-01-12 15:25:54 +0100

    Initial commit

---
+++ src/test/java/at/technikum/passwordgenerator/PasswordGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.passwordgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
